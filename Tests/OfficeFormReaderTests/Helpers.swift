396637a41065d56269be4ac8f8f81e267d34f3a5
Hugh Bellamy <hughbellars@gmail.com>
2020-12-16 14:28:36 +0000

    Fix whitespace

--- Tests/OfficeFormReaderTests/Helpers.swift
+++ Tests/OfficeFormReaderTests/Helpers.swift
@@ -1,6 +1,6 @@
 //
 //  Helpers.swift
-//  
+//
 //
 //  Created by Hugh Bellamy on 19/10/2020.
 //
@@ -11,4 +11,3 @@ public func getData(name: String, fileExtension: String) throws -> Data {
     let url = Bundle.module.url(forResource: name, withExtension: fileExtension)!
     return try Data(contentsOf: url)
 }
-
